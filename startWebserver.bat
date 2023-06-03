04315c6b3b32a29dde44e934cf6bd8cfb4b457fd
Mojmir Horvath <horvathmojmir@gmail.com>
2023-06-03 18:07:07 +0200

    price changes

--- startWebserver.bat
+++ startWebserver.bat
@@ -9,7 +9,7 @@ if %ERRORLEVEL% GTR 0 (
 )
 
 
-set "FEUERWEHR_APP_PASSWORD=FF-Weiden"
+set "FEUERWEHR_APP_PASSWORD=admin"
 set "FEUERWEHR_APP_USERNAMES=admin,Roman,Thomas,Mojmir,Guest,Marco,Stefan,Lea,Chrisoph,Vanessa,Elisa,Wachtler V,Bitter Ch,Steiner Th, Nolz S, Nolz B, Kerstin, Nolz H,Philip,Wachtler R, Krikler J, Schwarz M,Timo"
 
 

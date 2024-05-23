d3d74d9043d9850cfbdba5a277714995c06f8943
LordMojmir <75137860+LordMojmir@users.noreply.github.com>
2024-05-23 10:52:09 +0200

    working on usernames list

--- startWebserver.bat
+++ startWebserver.bat
@@ -11,10 +11,7 @@ if %ERRORLEVEL% GTR 0 (
 
 set "FEUERWEHR_APP_PASSWORD=admin"
 set "FEUERWEHR_APP_USERNAMES=admin,Roman,Thomas,Mojmir,Guest,Marco,Stefan,Lea,Chrisoph,Vanessa,Elisa,Wachtler V,Bitter Ch,Steiner Th,Nolz S,Nolz B,Kerstin,Nolz H,Philip,Wachtler R,Krikler J,Schwarz M,Timo"
-
-
  
-
 node %PROJECT_ROOT%src\webserver\start.js
 
 if errorlevel 1 pause
